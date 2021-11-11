9e4c90571aff6aea299121ecd03461047f9cc067
Sherin Varughese <shvarugh@redhat.com>
2021-11-11 15:13:00 -0500

    update req.conf folder

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 # Copyright (c) 2021 Red Hat, Inc.
 # Copyright Contributors to the Open Cluster Management project
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./opt/app-root/search-api/sslcert/searchapi.key -out ./opt/app-root/search-api/sslcert/searchapi.crt -config req.conf -extensions 'v3_req'
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./opt/app-root/search-api/sslcert/searchapi.key -out ./opt/app-root/search-api/sslcert/searchapi.crt -config ./opt/app-root/search-api/sslcert/req.conf -extensions 'v3_req'
